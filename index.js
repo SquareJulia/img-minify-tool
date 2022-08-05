2aaea2677952e8cc19a16e70318f98ee0f4eafda
zhusiyi02 <zhusiyi02@meituan.com>
2022-08-05 15:16:04 +0800

    feat: add plugin optipng

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const path = require('path');
-const fs = require('fs');
-const imagemin = require('imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminGifsicle = require('imagemin-gifsicle');
-const params = require('minimist')(process.argv.slice(2));
+const path = require("path");
+const fs = require("fs");
+const imagemin = require("imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminGifsicle = require("imagemin-gifsicle");
+const imageminOptipng = require("imagemin-optipng");
+const params = require("minimist")(process.argv.slice(2));
 
 //获取执行任务的路径
-const {
-  bundlePath
-} = params;
-console.log('------------------------ 开始执行图片压缩任务 ------------------------\n');
+const { bundlePath } = params;
+console.log(
+  "------------------------ 开始执行图片压缩任务 ------------------------\n"
+);
 console.log(`-> 本次压缩任务目录为：${bundlePath}\n`);
 //图片压缩函数
 (async () => {
@@ -19,17 +20,17 @@ console.log(`-> 本次压缩任务目录为：${bundlePath}\n`);
   //存放压缩后的图片体积
   let afterSizeList = [];
   //字节转kb方法
-  const byteToKb = (byte, unit = 'KB') => {
+  const byteToKb = (byte, unit = "KB") => {
     return `${(byte / 1024).toFixed(2)}${unit}`;
-  }
-  const savedSizeRate = (beforeSize, afterSize, unit = '%') => {
-    const savedRate = -(beforeSize - afterSize) * 100 / beforeSize;
+  };
+  const savedSizeRate = (beforeSize, afterSize, unit = "%") => {
+    const savedRate = (-(beforeSize - afterSize) * 100) / beforeSize;
     return `${savedRate.toFixed(1)}${unit}`;
-  }
+  };
   //遍历某个文件夹下所有的XX文件
   async function findImageAndLetItSmall({
     bundlePath,
-    EXT_NAME = /^\.(jpg|jpeg|png|gif)$/i
+    EXT_NAME = /^\.(jpg|jpeg|png|gif)$/i,
   }) {
     const pathList = fs.readdirSync(bundlePath);
     for (let i = 0; i < pathList.length; i++) {
@@ -40,7 +41,7 @@ console.log(`-> 本次压缩任务目录为：${bundlePath}\n`);
       if (statInfo.isDirectory()) {
         await findImageAndLetItSmall({
           bundlePath: childCatalogOrFilePath,
-          EXT_NAME
+          EXT_NAME,
         });
       } else {
         if (EXT_NAME.test(path.extname(childCatalogOrFilePath))) {
@@ -53,7 +54,7 @@ console.log(`-> 本次压缩任务目录为：${bundlePath}\n`);
             plugins: [
               imageminMozjpeg({
                 quality: 80, //质量过低， 会影响图片的视觉质量
-                smooth: 90
+                smooth: 90,
               }),
               imageminPngquant({
                 quality: [0.9, 0.95],
@@ -62,25 +63,37 @@ console.log(`-> 本次压缩任务目录为：${bundlePath}\n`);
               }),
               imageminGifsicle({
                 interlaced: true, //隔行扫描
-                quality: 90
+                quality: 90,
+              }),
+              imageminOptipng({
+                optimizationLevel:3,
               })
-            ]
+            ],
           });
           const afterSize = fs.statSync(childCatalogOrFilePath).size;
 
           afterSizeList = [...afterSizeList, afterSize];
-          console.log(`${childCatalogOrFilePath} -> 压缩完成（${byteToKb(beforeSize)} => ${byteToKb(afterSize)}[${savedSizeRate(beforeSize, afterSize)}]）`);
+          console.log(
+            `${childCatalogOrFilePath} -> 压缩完成（${byteToKb(
+              beforeSize
+            )} => ${byteToKb(afterSize)}[${savedSizeRate(
+              beforeSize,
+              afterSize
+            )}]）`
+          );
         }
       }
     }
   }
 
   await findImageAndLetItSmall({
-    bundlePath
+    bundlePath,
   });
   if (!beforeSizeList.length) {
     console.log(`-> 任务目录下未检测到gif、png、jpg、jpeg等图片！\n`);
-    console.log('------------------------ 任务正常终止, 下次见~ ------------------------');
+    console.log(
+      "------------------------ 任务正常终止, 下次见~ ------------------------"
+    );
     return;
   }
   //压缩前总体积
@@ -93,6 +106,12 @@ console.log(`-> 本次压缩任务目录为：${bundlePath}\n`);
   console.log(`-> 共压缩图片${beforeSizeList.length}张`);
   console.log(`-> 压缩前总体积${byteToKb(beforeSizeCount)}`);
   console.log(`-> 压缩后总体积${byteToKb(afterSizeCount)}`);
-  console.log(`-> 节约体积${(saveCount * 100 / beforeSizeCount).toFixed(2)}%（共${byteToKb(saveCount)}）\n`);
-  console.log('------------------------ 图片压缩任务执行完毕, 下次见~ ------------------------');
+  console.log(
+    `-> 节约体积${((saveCount * 100) / beforeSizeCount).toFixed(
+      2
+    )}%（共${byteToKb(saveCount)}）\n`
+  );
+  console.log(
+    "------------------------ 图片压缩任务执行完毕, 下次见~ ------------------------"
+  );
 })();
